531b033accf6d1c5085b9152a29aaf5a0e0578c1
William De Souza Fernandes <William.Fernandes@proway.treina>
2022-06-29 13:04:05 -0300

    [change] changed text.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'asdasdasdasdy',
+      title: 'yuuki mika killby',
       theme: ThemeData(
         
         primarySwatch: Colors.blue,
